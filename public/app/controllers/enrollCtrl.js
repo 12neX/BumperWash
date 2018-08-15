92d74ed9b46b6a4b9bc866bc53c49162f5467168
kushal <k1u2s3h4a5l6@gmail.com>
2018-08-16 02:23:53 +0530

    commit before hosting

---
+++ public/app/controllers/enrollCtrl.js
@@ -0,0 +1,36 @@
+angular.module('enrollCtrl',[])
+
+.controller('enrollCtrl',['$scope','$http',function($scope,$http){
+	var app = this;
+		console.log("inside enrollCtrl");
+
+	$(document).ready(function(){
+
+
+//Enroll now form
+        $("#loader1").hide();
+        $("#success-alert1").hide();
+        $("#failure-alert1").hide();
+        //route to send data from contact form
+        app.enrollUser= function(enrollData){
+            $("#loader").show();
+            $http.post('/api/enroll', this.enrollData).then(function(response){
+                console.log(response);
+                $('#enrollform').trigger("reset");
+                $("#loader1").hide();
+                $("#success-alert1").fadeTo(2000, 500).slideUp(500, function(){
+                $("#success-alert1").slideUp(500);
+            });   
+            },function(response){
+                console.log(response);
+                $('#enrollform').trigger("reset");
+                $("#loader1").hide();
+                $("#failure-alert1").fadeTo(2000, 500).slideUp(500, function(){
+                $("#failure-alert1").slideUp(500);
+            });
+            }
+        )};
+ 	});
+
+}]);
+		 
